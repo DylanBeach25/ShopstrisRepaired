c404ab830c0a410273799870e6f3c6e72c1959c2
DylanBeach25 <148595534+DylanBeach25@users.noreply.github.com>
2023-12-01 01:03:48 -0800

    Finishing basic designs for layouts and intent factory

--- app/src/main/java/com/example/myapplication/CreateUser.java
+++ app/src/main/java/com/example/myapplication/CreateUser.java
@@ -5,19 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.example.myapplication.databinding.ActivityCreateUserBinding;
+import com.example.myapplication.databinding.ActivityMainBinding;
 
 public class CreateUser extends AppCompatActivity {
 
     EditText mUser_name;
     EditText mPassword;
+    EditText mConfirmPassword;
     Button mRegister;
+
+    ActivityCreateUserBinding mCreateUserBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
+
+        mCreateUserBinding = ActivityCreateUserBinding.inflate(getLayoutInflater());
+        View view = mCreateUserBinding.getRoot();
+
+        setContentView(view);
+
+        mRegister = mCreateUserBinding.button;
+        mConfirmPassword = mCreateUserBinding.confirmPassword;
+        mPassword = mCreateUserBinding.password;
+        mUser_name = mCreateUserBinding.userName;
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = MainActivity.getIntent(getApplicationContext());
+                startActivity(intent);
+            }
+        });
+
     }
 
     public static Intent getIntent(Context context){
