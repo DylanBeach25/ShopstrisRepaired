59b041f1635f9ce356226f8447391d1543f85253
DylanBeach25 <148595534+DylanBeach25@users.noreply.github.com>
2023-12-09 18:57:11 -0800

    Shared preference passing into current orders tab

--- app/src/main/java/com/example/myapplication/CurrentOrders.java
+++ app/src/main/java/com/example/myapplication/CurrentOrders.java
@@ -6,7 +6,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.myapplication.databinding.ActivityCurrentOrdersBinding;
 
@@ -15,21 +17,29 @@ import java.util.List;
 
 public class CurrentOrders extends AppCompatActivity {
 
-
-
+    public static final String SHARED_PREFS = "sharedPrefs";
+    //ActivityCurrentOrdersBinding mCurrentOrdersBinding;
+    //TextView mUserNameOrders;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_current_orders);
 
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
+
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        TextView textView = findViewById(R.id.orders_username);
+
+        textView.setText(sharedPreferences.getString("name",""));
 
         List<CartEntity> carts = new ArrayList<>();
+        /*
         carts.add(new CartEntity(1,"test1"));
         carts.add(new CartEntity(2,"test2"));
         carts.add(new CartEntity(3,"test3"));
         carts.add(new CartEntity(4,"test4"));
+         */
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new MyAdapter(getApplicationContext(),carts));
